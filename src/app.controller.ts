c21eb566c0809e59627af089c89d3d7dec54abac
Kevin <kevin.arian3003@gmail.com>
2021-08-08 09:54:55 -0300

    removed and replaced old auth implementation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards, Req } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @UseGuards(AuthGuard('jwt'))
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Req() request: Request): string {
+    console.log('User', JSON.stringify(request['user']));
+    return 'Hello ' + JSON.stringify(request['user']) + '!';
   }
 }
