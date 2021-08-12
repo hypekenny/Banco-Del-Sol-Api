9338484d2da02878b77838957efb57e538313598
Santiago <smferro@gmail.com>
2021-08-12 14:57:22 -0300

    starting services and controllers

--- Transactions_microService/src/transactions/transactions.controller.ts
+++ Transactions_microService/src/transactions/transactions.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Res,
+  HttpStatus,
+  Put,
+  Body,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
+import { TransactionsService } from './transactions.service';
 
 @Controller('transactions')
-export class TransactionsController {}
+export class TransactionsController {
+  constructor(private transactionService: TransactionsService) {}
+
+
+@Get()
+async getTransactions(@Res() res, @Req req) {
+    try {
+        const transactions = await this.transactionService.
+    }
+}
+
+
+@Put()
+
+
+}
--- Transactions_microService/src/transactions/transactions.service.ts
+++ Transactions_microService/src/transactions/transactions.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { TransactionDocument } from './transactions.model';
 
 @Injectable()
-export class TransactionsService {}
+export class TransactionsService {
+  constructor(
+    @InjectModel('Transactions')
+    private readonly transactionModel: Model<TransactionDocument>,
+  ) {}
+
+  // async getTransactions(email: string);
+}
