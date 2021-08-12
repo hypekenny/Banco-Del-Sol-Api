acc26a464b38979272e6a3ba7b31de248da2a915
Santiago <smferro@gmail.com>
2021-08-12 19:48:59 -0300

    errors solved

---
+++ Transactions_microService/src/account/account.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Account, AccountDocument } from './account.model';
+import { Model } from 'mongoose';
+
+const createCvu = () => {
+  const a = Math.random() * 10 ** 23;
+  const b = a.toLocaleString('fullwide', { useGrouping: false });
+  return b;
+};
+
+@Injectable()
+export class AccountService {
+  constructor(
+    @InjectModel('Account')
+    private readonly accountModel: Model<AccountDocument>,
+  ) {}
+
+  async getAccount(email: string): Promise<Account> {
+    try {
+      const findAccount = await this.accountModel.findOne({
+        email: email,
+      });
+      return findAccount;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async createAccount(account: Account): Promise<Account> {
+    try {
+      const newAccount = new this.accountModel({
+        ...account,
+        email: account.email,
+        cvu: createCvu(),
+      });
+      return await newAccount.save();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async updateAccount(email: string, newBalance: number) {
+    try {
+      const findAccount = await this.accountModel.findOneAndUpdate(
+        {
+          email: email,
+        },
+        { balance: newBalance },
+        {
+          new: true,
+          useFindAndModify: false,
+        },
+      );
+      return findAccount;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
