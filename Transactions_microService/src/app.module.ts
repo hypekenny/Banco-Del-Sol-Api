d58dcee60911114fb2fcb06c4c566c5c3ebaa40f
Santiago <smferro@gmail.com>
2021-08-12 13:04:57 -0300

    feat/added transactions microservice

---
+++ Transactions_microService/src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/banco-del-sol', {
+      autoCreate: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
