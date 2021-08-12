0bdefb2c068c580d20b985c254fadf8b72b424ca
Santiago <smferro@gmail.com>
2021-08-12 13:13:10 -0300

    added model transaction

--- Transactions_microService/src/app.module.ts
+++ Transactions_microService/src/app.module.ts
@@ -2,14 +2,17 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TransactionsService } from './transactions/transactions.service';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/banco-del-sol', {
       autoCreate: true,
     }),
+    TransactionsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TransactionsService],
 })
 export class AppModule {}
