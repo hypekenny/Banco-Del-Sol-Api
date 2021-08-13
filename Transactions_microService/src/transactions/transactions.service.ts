a4e3659adfa8039d4b147731c0e54bf5efd1c4b6
Santiago <smferro@gmail.com>
2021-08-12 21:02:03 -0300

    last changes

--- Transactions_microService/src/transactions/transactions.service.ts
+++ Transactions_microService/src/transactions/transactions.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { AccountService } from '../account/account.service';
-import { AccountDocument } from '../account/account.model';
 import { TransactionDocument, Transaction } from './transactions.model';
 
 @Injectable()
@@ -10,7 +9,6 @@ export class TransactionsService {
   constructor(
     @InjectModel('Transaction')
     private readonly transactionModel: Model<TransactionDocument>,
-    private readonly accountModel: Model<AccountDocument>,
     private accountService: AccountService,
   ) {}
 
