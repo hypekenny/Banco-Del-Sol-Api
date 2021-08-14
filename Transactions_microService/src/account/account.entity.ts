7af8b35aad3e8de79f738501ad9c1cab038e6c0c
nicolas <cnico_97@hotmail.com>
2021-08-14 17:18:32 -0300

    trying things that also didin`t work

--- Transactions_microService/src/account/account.entity.ts
+++ Transactions_microService/src/account/account.entity.ts
@@ -35,35 +35,3 @@ export class Accounts {
   @Column()
   cvu: string;
 }
-
-// import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-// import { Document } from 'mongoose';
-
-// export type AccountDocument = Account & Document;
-
-// export type balanceType = {
-//   amount: number;
-//   history: Array<transactionType>;
-// };
-
-// export type transactionType = {
-//   senderEmail: string;
-//   receiverEmail: string;
-//   type: string;
-//   value: number;
-//   date: Date;
-// };
-
-// @Schema()
-// export class Account {
-//   @Prop({ required: true, unique: true })
-//   email: string;
-
-//   @Prop({ required: true, type: Object })
-//   balance: balanceType;
-
-//   @Prop({ required: true })
-//   cvu: string;
-// }
-
-// export const AccountSchema = SchemaFactory.createForClass(Account);
--- Transactions_microService/src/account/account.service.ts
+++ Transactions_microService/src/account/account.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Accounts, AccountDocument, transactionType } from './account.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, Connection } from 'typeorm';
+import { Repository, getConnection, QueryBuilder } from 'typeorm';
 
 const createCvu = () => {
   const a = Math.random() * 10 ** 23;
@@ -14,7 +14,6 @@ export class AccountService {
   constructor(
     @InjectRepository(Accounts)
     private readonly accountRepository: Repository<AccountDocument>,
-    private readonly connection: Connection,
   ) {}
 
   async getAccount(email: string): Promise<Accounts> {
@@ -43,38 +42,67 @@ export class AccountService {
   }
 
   async updateAccount(tran: transactionType) {
-    const queryRunner = this.connection.createQueryRunner();
+    const connection = getConnection();
+    const queryRunner = connection.createQueryRunner();
     await queryRunner.connect();
     await queryRunner.startTransaction();
+    const senderAcc = await this.accountRepository.findOne({
+      email: tran.senderEmail,
+    });
+    senderAcc.balance.amount -= tran.value;
+    senderAcc.balance.history.push(tran);
+    const receiverAcc = await this.accountRepository.findOne({
+      email: tran.receiverEmail,
+    });
+    receiverAcc.balance.amount += tran.value;
+    receiverAcc.balance.history.push(tran);
     try {
-      if (tran.receiverEmail === tran.senderEmail) {
-        const findSender = await this.accountRepository.findOne({
-          email: tran.senderEmail,
-        });
-        findSender.balance.amount += tran.value;
-        findSender.balance.history.push(tran);
-        await queryRunner.manager.save(findSender);
-        return findSender;
-      } else {
-        const findSender = await this.accountRepository.findOne({
-          email: tran.senderEmail,
-        });
-        findSender.balance.amount -= tran.value;
-        findSender.balance.history.push(tran);
-        await queryRunner.manager.save(findSender);
-        const findReceiver = await this.accountRepository.findOne({
-          email: tran.receiverEmail,
-        });
-        findReceiver.balance.amount += tran.value;
-        findReceiver.balance.history.push(tran);
-        await queryRunner.manager.save(findReceiver);
-        return findSender;
-      }
+      await queryRunner.manager.save(senderAcc);
+      await queryRunner.manager.save(receiverAcc);
+      await queryRunner.commitTransaction();
     } catch (error) {
-      console.log(error);
+      console.log('rollback', error);
       await queryRunner.rollbackTransaction();
     } finally {
       await queryRunner.release();
     }
   }
 }
+
+// async updateAccount(tran: transactionType) {
+//   const queryRunner = this.connection.createQueryRunner();
+//   await queryRunner.connect();
+//   await queryRunner.startTransaction();
+//   try {
+//     if (tran.receiverEmail === tran.senderEmail) {
+//       const findSender = await this.accountRepository.findOne({
+//         email: tran.senderEmail,
+//       });
+//       findSender.balance.amount += tran.value;
+//       findSender.balance.history.push(tran);
+//       await queryRunner.manager.save(findSender);
+//       return findSender;
+//     } else {
+//       const findSender = await this.accountRepository.findOne({
+//         email: tran.senderEmail,
+//       });
+//       findSender.balance.amount -= tran.value;
+//       findSender.balance.history.push(tran);
+//       await queryRunner.manager.save(findSender);
+//       const findReceiver = await this.accountRepository.findOne({
+//         email: tran.receiverEmail,
+//       });
+//       findReceiver.balance.amount += tran.value;
+//       findReceiver.balance.history.push(tran);
+//       await queryRunner.manager.save(findReceiver);
+//       await queryRunner.commitTransaction();
+//       // throw new Error();
+//       return findSender;
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     await queryRunner.rollbackTransaction();
+//   } finally {
+//     await queryRunner.release();
+//   }
+// }
