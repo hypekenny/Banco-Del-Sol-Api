3341ff5d35c6503bb2646962f967c481f489a59f
Santiago <smferro@gmail.com>
2021-08-18 20:59:21 -0300

    transactions controller

--- Transactions_microService/src/transactions/transactions.controller.ts
+++ Transactions_microService/src/transactions/transactions.controller.ts
@@ -22,10 +22,13 @@ export class TransactionsController {
   async newTransaction(@Res() res, @Body() body) {
     try {
       const succeeded = await this.transactionService.createTransaction(body);
+      if (succeeded === undefined) throw new Error();
       return res.send(succeeded);
     } catch (error) {
       console.error(error);
-      return res.status(400);
+      return res.status(400).send({
+        message: 'Not found',
+      });
     }
   }
 }
