dd83d5f02c1612983d855bdd7f5117f08b280442
nicolas <cnico_97@hotmail.com>
2021-08-21 00:04:33 -0300

    enable cors * for the moment

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
-  app.enableCors({ origin: 'http://localhost:19006' });
+  app.enableCors({ origin: '*' });
   await app.listen(3001);
 }
 bootstrap();
