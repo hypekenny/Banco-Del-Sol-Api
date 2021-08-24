e90265049135f7f460318f6b95d886aca885ec33
Santiago <smferro@gmail.com>
2021-08-23 22:05:46 -0300

    user service test

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 
+class UserServiceMock {
+  getUserByEmail(email: string) {
+    return {
+      email: 'john@hotmail.com',
+      dni: 22321654,
+      name: 'John',
+      lastName: 'Doe',
+      birthdate: '12/4/1990',
+      phoneNumber: '1121547563',
+    };
+  }
+}
+
 describe('UserService', () => {
-  let service: UserService;
+  let app: TestingModule;
+  let user: UserService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+    const UserServiceProvider = {
+      provide: UserService,
+      useClass: UserServiceMock,
+    };
+    app = await Test.createTestingModule({
+      providers: [UserServiceProvider],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    user = app.get<UserService>(UserService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should get a user by email', async () => {
+    const expectedRes = {
+      email: 'john@hotmail.com',
+      dni: 22321654,
+      name: 'John',
+      lastName: 'Doe',
+      birthdate: '12/4/1990',
+      phoneNumber: '1121547563',
+    };
+    const service = await user.getUserByEmail('john@hotmail.com');
+    expect(service).toEqual(expectedRes);
   });
 });
