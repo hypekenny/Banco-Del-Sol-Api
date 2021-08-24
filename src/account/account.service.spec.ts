0838d95d2f51da1235bf15516b6150d18c214336
Santiago <smferro@gmail.com>
2021-08-23 22:07:22 -0300

    account service test

--- src/account/account.service.spec.ts
+++ src/account/account.service.spec.ts
@@ -2,17 +2,53 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AccountService } from './account.service';
 
 describe('AccountService', () => {
-  let service: AccountService;
+  class AccountServiceMock {
+    getAccount(email: string) {
+      return {
+        email: 'john@hotmail.com',
+        balance: { amount: 200, history: [] },
+        cvu: '221132123213',
+      };
+    }
+  }
+  let app: TestingModule;
+  let account: AccountService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AccountService],
+    const AccountServiceProvider = {
+      provide: AccountService,
+      useClass: AccountServiceMock,
+    };
+    app = await Test.createTestingModule({
+      providers: [AccountServiceProvider],
     }).compile();
 
-    service = module.get<AccountService>(AccountService);
+    account = app.get<AccountService>(AccountService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should get the account', async () => {
+    const expectedRes = {
+      email: 'john@hotmail.com',
+      balance: { amount: 200, history: [] },
+      cvu: '221132123213',
+    };
+    const service = await account.getAccount('john@hotmail.com');
+    expect(service).toEqual(expectedRes);
   });
+
+  /*  it('should create the account', async () => {
+    const expectedRes = {
+      email: 'john@hotmail.com',
+      balance: { amount: 0, history: [] },
+      cvu: '',
+      condition: '',
+    };
+    const service = await account.createAccount({
+      email: 'john@hotmail.com',
+      cvu: '',
+      balance: { amount: 0, history: [] },
+      condition: 'active',
+    });
+    expect(service).toEqual(expectedRes);
+  }); */
 });
